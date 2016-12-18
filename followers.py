186acf3febc710ab0be1c7835a4bbc02c9dfe3a2
cumaliaba <abalik@itu.edu.tr>
2016-12-18 14:01:47 +0200

    Move the init follower table from server.py

--- followers.py
+++ followers.py
@@ -4,6 +4,23 @@ import datetime
 class followers:
     def __init__(self, app):
         self.app = app
+        
+    def initialize_table(self):
+        with dbapi2.connect(self.app.config['dsn']) as connection:
+            try:
+                cursor = connection.cursor()
+                cursor.execute(""" CREATE TABLE IF NOT EXISTS FOLLOWER(
+                PERSON_ID INTEGER NOT NULL REFERENCES USERS (USER_ID) ON DELETE CASCADE,
+                FOLLOWER_ID INTEGER NOT NULL REFERENCES USERS (USER_ID) ON DELETE CASCADE,
+                GROUP_NAME VARCHAR(50),
+                DATE DATE NOT NULL,
+                PRIMARY KEY(PERSON_ID,FOLLOWER_ID)
+                )""")
+                connection.commit()
+            except dbapi2.DatabaseError:
+                connection.rollback()
+            finally:
+               connection.commit()
 
     def show_followers(self):
         with dbapi2.connect(self.app.config['dsn']) as connection:
