5feaaebe21153e9245ed55ac2be54a4daddb58d9
Utku Öner <oner.utku@yahoo.com>
2016-12-19 02:46:21 +0200

    Updated hypeline.py to add follower function to the hypeline

--- hypeline.py
+++ hypeline.py
@@ -31,3 +31,20 @@ class Hypeline:
                 connection.rollback()
             finally:
                 connection.commit()
+                
+    def List_Hypes_Hypeline(self, username):
+        with dbapi2.connect(self.app.config['dsn']) as connection:
+            try:
+                cursor = connection.cursor()
+                query = """SELECT USERNAME, TEXT, TOPIC, HYPES.DATE FROM USERS INNER JOIN FOLLOWER
+                        ON USERS.USER_ID = FOLLOWER.FOLLOWER_ID INNER JOIN
+                        HYPES ON HYPES.USER_ID = FOLLOWER.FOLLOWER_ID
+                        WHERE FOLLOWER.PERSON_ID = %s ORDER BY DATE DESC"""
+                cursor.execute(query, (username,))
+                hypes = cursor.fetchall()
+                return hypes
+            except dbapi2.DatabaseError:
+                connection.rollback()
+            finally:
+                connection.commit()
+
